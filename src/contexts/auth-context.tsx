1c21f8213af3b35d8a28a742213de42306f76e8f
Grazeweb <grazeweb@gmail.com>
2026-05-28 15:14:45 -0600

    1

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -11,7 +11,7 @@ interface AuthContextType {
   loading: boolean;
   logout: () => Promise<void>;
 }
-w23445
+\
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
