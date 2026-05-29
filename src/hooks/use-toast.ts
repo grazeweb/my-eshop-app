53068c08045c261e8ada615ef03cb04921e0ff3e
Grazeweb <grazeweb@gmail.com>
2026-05-29 14:17:07 -0600

    2

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -17,7 +17,7 @@ type ToasterToast = ToastProps & {
   description?: React.ReactNode
   action?: ToastActionElement
 }
-1
+
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
